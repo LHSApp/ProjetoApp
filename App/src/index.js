4428e7d6dac084acefde5116c9464e5cfb5e360e
LHSApp <leandrosantos020788@gmail.com>
2020-10-05 13:27:19 -0300

    Update

--- App/src/index.js
+++ App/src/index.js
@@ -1,11 +1,47 @@
 const express = require('express');
 const { request } = require('express');
 const app =express();
-app.get('/', (request, response) =>{
+app.get('/projects', (request, response) =>{
 
-  return response.json({message: 'Hello Word '});
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+  ]);
 
 });
+
+app.post('/projects', (request, response) =>{
+
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+
+});
+
+
+app.put('/projects/:id', (request, response) =>{
+
+  return response.json([
+    'Projeto 4',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+
+});
+
+app.delete('/projects/:id', (request, response) =>{
+
+  return response.json([
+   
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+
+});
+
+
 
 app.listen(3333, () => {
 
