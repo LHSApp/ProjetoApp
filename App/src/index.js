c744e3a0205153d13209d1ae0caa156633d13dd8
LHSApp <leandrosantos020788@gmail.com>
2020-10-05 13:03:14 -0300

    Iniciando o Back-end

--- App/src/index.js
+++ App/src/index.js
@@ -3,8 +3,11 @@ const { request } = require('express');
 const app =express();
 app.get('/', (request, response) =>{
 
-  return response.json({message: 'Hello Word'});
+  return response.json({message: 'Hello Word '});
 
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+
+  console.log('🚀 Back-end satrtted!')
+});
